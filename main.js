ebf7b31df7a124ead7da2b551a6c18e38a02f24a
Niveditha-star <82389099+Niveditha-star@users.noreply.github.com>
2022-06-02 19:55:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+<script src="quiz_game_page.js"></script>
+<h4 id="player1_name"></h4> <span id="player1_score"></span>
+<br>
+<h4 id="player2_name"></h4> <span id="player2_score"></span>
+<div class="container">
+    </div></body>
+</html>
